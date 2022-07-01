5e4ab6a073ad16a976d69cdb13048c556982e60e
MoikeCheck <mikewheatley@hotmail.co.uk>
2022-07-01 23:38:44 +0100

    db updated

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,7 +1,7 @@
 <?php 
    
  //connect to the data base
-$conn = mysqli_connect ('eu-cdbr-west-03.cleardb.net', 'bb8d4bf4bec043', 'c1fddf66', 'recipes');
+$conn = mysqli_connect ('eu-cdbr-west-03.cleardb.net', 'bb8d4bf4bec043', 'c1fddf66', 'heroku_fa69b89ace347e5');
 
 //check connect 
 if(!$conn){
